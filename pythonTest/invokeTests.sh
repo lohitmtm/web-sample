fcd70ff6c495d5d8ac7a49f6b32782f21e2f1ecf
lohitmtm <63779116+lohitmtm@users.noreply.github.com>
2020-06-22 19:27:34 +0530

    Update invokeTests.sh

--- pythonTest/invokeTests.sh
+++ pythonTest/invokeTests.sh
@@ -3,4 +3,5 @@
 # Enable the the python 2.7 which is required to execuet selenium 3.141.0 web driver code
 #source /opt/rh/python27/enable
 # Add test case files below
-/usr/bin/python ./calculatorTest.py
+# /usr/bin/python ./calculatorTest.py
+C:\Users\132803\AppData\Local\Programs\Python\Python38-32\python.exe ./calculatorTest.py
