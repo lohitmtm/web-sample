135fa838a5fe5569144df3892b560b4c641bf22e
lohitmtm <63779116+lohitmtm@users.noreply.github.com>
2020-06-22 21:56:25 +0530

    Update invokeTests.sh

--- pythonTest/invokeTests.sh
+++ pythonTest/invokeTests.sh
@@ -1,6 +1,2 @@
-# include geckodriver path in the user profile
-#export PATH=$PATH:/opt/geckoDriver/
-# Enable the the python 2.7 which is required to execuet selenium 3.141.0 web driver code
-#source /opt/rh/python27/enable
-# Add test case files below
- /usr/bin/python ./calculatorTest.py
+# /usr/bin/python ./calculatorTest.py
+C:\Users\132803\AppData\Local\Programs\Python\Python38-32\python.exe ./calculatorTest.py
